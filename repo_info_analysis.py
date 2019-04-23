d25ecbf0b9e6ea4362b5378006797896390fda9d
Hao He <hehao1998@pku.edu.cn>
2019-04-23 18:43:39 +0800

    Updated repo info analysis code

--- repo_info_analysis.py
+++ repo_info_analysis.py
@@ -1,6 +1,6 @@
 '''
 Print and plot analysis results for downloaded GitHub repository information
-Plots will be in /temp/plots folder
+Also, output a CSV file for repository statistics
 Assume the information is in 'temp/repo_info.json', run repo_info_download.py first to download that
 
 Created By He, Hao on 2019/04/03
@@ -9,12 +9,10 @@ Created By He, Hao on 2019/04/03
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
-if __name__ == '__main__':
-    repo_info = []
-    with open('temp/repo_info.json', 'r') as f:
-        repo_info = json.load(f)
 
+def plot_lang_freq(repo_info):
     # Count language freqencies
     language_freq = {}
     for repo in repo_info:
@@ -42,21 +40,60 @@ if __name__ == '__main__':
     plt.title('Top 20 Languages among 1000 Most Starred GitHub Repositories')
     plt.xlabel('# of Repositories')
     plt.show()
+    return
 
-    repo_info_selected = []
-    with open('temp/repo_info_selected.json', 'r') as f:
-        repo_info_selected = json.load(f)
 
+def plot_repo_size_dist(repo_info):
     xaxis = []
     yaxis = []
-    for item in repo_info_selected:
+    for item in repo_info:
         xaxis.append(item['size'] / 1024)
     xaxis = sorted(xaxis)
     for i in range(0, len(xaxis)):
         yaxis.append((i + 1) / len(xaxis))
     plt.plot(xaxis, yaxis)
-    plt.xlabel('Reposirory Size(MB)')
+    plt.xlabel('Repository Size(MB)')
     plt.title('CDF of Repository Size')
     plt.ylim(0, 1)
     plt.xlim(0)
     plt.show()
+    return
+
+
+def to_csv(repo_info, output_path):
+    fieldnames = ['full_name', 'language', 'commits', 'stars', 'forks',
+                  'contributors', 'open_issues', 'lines_of_code', 'lines_of_comment']
+
+    # Output interesting repository metrics to CSV
+    with open('temp/repo_info.csv') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for item in repo_info:
+            writer.writerow({
+                'full_name': item['full_name'],
+                'language': item['language'],
+                'commits': item['commit_count'],
+                'stars': item['stargazers_count'],
+                'forks': item['forks'],
+                'contributors': item['contributor_count'],
+                'open_issues': item['open_issues_count'],
+                'src_files': 0,
+                'lines_of_code': 0,
+                'lines_of_comment': 0,
+            })
+    return
+
+
+if __name__ == '__main__':
+    repo_info = []
+    with open('temp/repo_info.json', 'r') as f:
+        repo_info = json.load(f)
+    repo_info_selected = []
+    with open('temp/repo_info_selected.json', 'r') as f:
+        repo_info_selected = json.load(f)
+
+    # plot_lang_freq(repo_info)
+    # plot_repo_size_dist(repo_info_selected)
+
+    to_csv(repo_info, 'temp/repo_info.csv')
+    to_csv(repo_info_selected, 'temp/repo_info_selected.csv')
--- repo_info_download.py
+++ repo_info_download.py
@@ -9,23 +9,21 @@ import json
 import time
 
 
-'''
-Returns a json object that contains information of GitHub repos returned by GitHub REST v3 API
-
-Example search url: https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc
-This URL collects GitHub Java project sorted by starts in descending order.
-Remember that:
-1. The results are returned in pages, so you have to fetch them page by page
-2. You are limited to send only 10 requests per minute
-3. You can only get up to 1000 search results
-
-Reference Documentation: 
-https://developer.github.com/v3/search/
-https://help.github.com/en/articles/searching-for-repositories
-'''
-
-
 def get_repolist_by_stars(num=30, lang=''):
+    '''
+    Returns a json object that contains information of GitHub repos returned by GitHub REST v3 API
+
+    Example search url: https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc
+    This URL collects GitHub Java project sorted by stars in descending order.
+    Remember that:
+    1. The results are returned in pages, so you have to fetch them page by page
+    2. You are limited to send only 10 requests per minute
+    3. You can only get up to 1000 search results
+
+    Reference Documentation: 
+    https://developer.github.com/v3/search/
+    https://help.github.com/en/articles/searching-for-repositories
+    '''
     url = 'https://api.github.com/search/repositories'
     params = {'q': 'stars:>1000', 'sort': 'stars',
               'order': 'desc', 'page': '1'}
@@ -51,11 +49,58 @@ def get_repolist_by_stars(num=30, lang=''):
     return repolist[0:num]
 
 
-if __name__ == '__main__':
-    languages = ['Java', 'C++', 'JavaScript', 'Python', 'Go']
+def add_commit_and_contributor_info(repolist):
+    '''
+    Add 'commit_count' and 'contributor_count' field to each repo object
+    the data is collected using other GitHub APIs
+    '''
 
+    print('Downloading commit information from GitHub...')
+    for repo in repolist:
+        url = 'https://api.github.com/repos/' + repo['full_name'] + '/commits'
+        response = requests.get(url)
+        # Example link: https://api.github.com/resource?page=5
+        last_link = response.links['last']['url']
+        first_page = response.json()
+        last_page_num = int(last_link.split('=')[1])
+        last_page_item_count = len(requests.get(
+            url, params={'page': last_page_num}).json())
+        repo['commit_count'] = (
+            last_page_num - 1) * len(first_page) + last_page_item_count
+        print(repo['full_name'] + ': ' +
+              str(repo['commit_count']) + ' commits')
+        # This rate is imposed by GitHub, and we send 2 requests each iteration
+        time.sleep(14)
+
+    # Why duplicate code like this?
+    print('Downloading contributor information from GitHub...')
+    for repo in repolist:
+        url = 'https://api.github.com/repos/' + \
+            repo['full_name'] + '/contributors'
+        response = requests.get(url)
+        last_link = response.links()['last']['url']
+        first_page = response.json()
+        last_page_num = int(last_link.split('=')[1])
+        last_page_item_count = len(requests.get(
+            url, params={'page': last_page_num}).json())
+        repo['contributor_count'] = (
+            last_page_num - 1) * len(first_page) + last_page_item_count
+        print(repo['full_name'] + ': ' +
+              str(repo['contributor_count']) + ' contributors')
+        # This rate is imposed by GitHub, and we send 2 requests each iteration
+        time.sleep(14)
+
+
+def download_overall_info():
+    '''
+    Download information of 1000 most starred information
+    '''
     print('---------- Downloading Overall Repository Information ----------')
     repolist = get_repolist_by_stars(num=1000)
+
+    # Get number of commits and number of contributors of this repository,
+    # which is not included in the response by default
+    add_commit_and_contributor_info(repolist)
 
     # Count language freqencies
     language_freq = {}
@@ -74,10 +119,23 @@ if __name__ == '__main__':
     with open('temp/repo_info.json', 'w') as file:
         json.dump(repolist, file)
 
+
+def download_by_language():
+    '''
+    Download reposiory information by language
+    '''
+    languages = ['Java', 'C++', 'JavaScript', 'Python', 'Go']
+
     print('---------- Downloading Top Repository Information By Language ----------')
     repolist = []
     for lang in languages:
-        print('Downloading repository by language ' + lang + '...')
+        print('Downloading repository in language ' + lang + '...')
         repolist.extend(get_repolist_by_stars(num=20, lang=lang))
+    add_commit_and_contributor_info(repolist)
     with open('temp/repo_info_selected.json', 'w') as file:
         json.dump(repolist, file)
+
+
+if __name__ == '__main__':
+    download_overall_info()
+    download_by_language()
