fba25f74f1ec9982cc5a36411219b9cfa6d49ede
Hao He <heh@pku.edu.cn>
2019-07-14 08:02:34 -0400

    Add more functionalities

---
+++ Working/BuildProjectCSV.py
@@ -0,0 +1,16 @@
+'''
+Using information from temp/FinalProjectsWithGitHubInfo.json, build a csv file
+Will query world of code for additional information
+The result will be saved to result/Projects.csv
+
+Author: He, Hao
+'''
+
+import json
+import csv
+import os
+
+if __name__ == '__main__':
+    project_into
+    with open('temp/FinalProjectsWithGitHubInfo.json', 'r'):
+        
--- Working/DownloadProjectMetadata.py
+++ Working/DownloadProjectMetadata.py
@@ -12,6 +12,7 @@ import requests
 import argparse
 import time
 import datetime
+import sys
 
 username = ''
 auth_token = ''
@@ -26,27 +27,42 @@ if __name__ == '__main__':
 
     project_list = []
     new_project_list = []
+    new_project_names = set()
     with open('temp/FinalProjects.json', 'r') as f:
         project_list = json.load(f)
+    if os.path.isfile('temp/FinalProjectsWithGitHubInfo.json'):
+        with open('temp/FinalProjectsWithGitHubInfo.json', 'r') as f:
+            new_project_list = json.load(f)
+            new_project_names = set([proj['full_name'] for proj in new_project_list])
 
     print('Retrieving info for {} projects...'.format(len(project_list)))
     for proj in project_list:
-        info_url = 'https://api.github.com/repos/{}'.format(proj['name'].replace('_', '/'))
-        r = requests.get(info_url, auth=(username, auth_token))
-        rate_remaining = int(r.headers['X-RateLimit-Remaining'])
-        if rate_remaining <= 0:
-            epoch_time = int(r.headers['X-RateLimit-Reset'])
-            sleep_time = epoch_time - time.time() + 20
-            print('Exceeding rate limit, sleeping for {} seconds'.format(sleep_time))
-            time.sleep(sleep_time)
-        if r.status_code != 200: # Errors
-            print('ERROR: Status code {} when accessing {}'.format(r.status_code, info_url))
+        if proj['name'].replace('_', '/') in new_project_names:
             continue
-        info = r.json()
-        new_project_list.append(info)
-        print('{}: Retrieved {}, {} requests remaining in this time period'
-            .format(datetime.datetime.now(), info_url, rate_remaining))
-        
+        try: 
+            info_url = 'https://api.github.com/repos/{}'.format(proj['name'].replace('_', '/'))
+            r = requests.get(info_url, auth=(username, auth_token))
+            rate_remaining = int(r.headers['X-RateLimit-Remaining'])
+            if rate_remaining <= 0:
+                epoch_time = int(r.headers['X-RateLimit-Reset'])
+                sleep_time = max(0, epoch_time - time.time() + 20)
+                print('Exceeding rate limit, sleeping for {} seconds'.format(sleep_time))
+                time.sleep(sleep_time)
+            if r.status_code != 200: # Errors
+                print('ERROR: Status code {} when accessing {}'.format(r.status_code, info_url))
+                continue
+            info = r.json()
+            new_project_list.append(info)
+            new_project_names.add(info['full_name'])
+            print('{}: Retrieved {}, {} requests remaining in this time period'
+                .format(datetime.datetime.now(), info_url, rate_remaining))
+        except:
+            print('Error when retrieving {}'.format(proj['name']), sys.exc_info()[0])
+        # Wrtie to file for every 100 requests
+        if len(new_project_list) % 100 == 0:
+            with open('temp/FinalProjectsWithGitHubInfo.json', 'w') as f:
+                f.write(json.dumps(new_project_list))
+            
     with open('temp/FinalProjectsWithGitHubInfo.json', 'w') as f:
         f.write(json.dumps(new_project_list))
     
