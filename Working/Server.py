4d60d2f8a0cf3bfd18f7be54e489ec6fdc64b5e5
Hao He <hehao1998@pku.edu.cn>
2019-07-16 16:10:55 +0800

    Added Server.py

---
+++ Working/Server.py
@@ -0,0 +1,10 @@
+'''
+A Custom Server over SSH Tunnel that can serve requests on WoC data
+For request and response documentation, see (TODO)
+
+Author: He, Hao
+'''
+
+if __name__ == '__main__':
+    
+
