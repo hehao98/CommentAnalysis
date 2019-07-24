ab7fad9d522e13b1db80f7fdf716b58e3093a5fe
Hao He <heh@pku.edu.cn>
2019-07-24 12:19:58 +0800

    Added job arguments to DownloadProjects.py

--- Working/DownloadProjects.py
+++ Working/DownloadProjects.py
@@ -12,14 +12,22 @@ from datetime import datetime
 from multiprocessing import Pool
 from termcolor import colored
 
+
 def run_proc(index, url, name):
     print(colored('Downloading Project {}...'.format(index), 'green'))
-    subprocess.call('git clone --depth=1 {}.git {}'.format(url, name), shell=True)
+    subprocess.call(
+        'git clone --depth=1 {}.git {}'.format(url, name), shell=True)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('csv_path', help='Path to the CSV file storing project information')
-    csv_path = parser.parse_args().csv_path
+    parser.add_argument(
+        'csv_path', help='Path to the CSV file storing project information')
+    parser.add_argument('-j', type=int, default=4,
+                        help='Number of Jobs (Default 4)')
+    args = vars(parser.parse_args())
+    csv_path = args['csv_path']
+    num_job = args['j']
 
     begin_time = datetime.now()
 
@@ -28,11 +36,10 @@ if __name__ == '__main__':
     if not os.path.exists('projects/'):
         os.mkdir('projects')
     os.chdir('projects')
-    
-    pool = Pool(12)
-    for index, row in projects.iterrows(): 
+
+    pool = Pool(num_job)
+    for index, row in projects.iterrows():
         pool.apply_async(run_proc, args=(index, row['url'], row['name']))
     pool.close()
     pool.join()
     print('Total running time: {}'.format(datetime.now() - begin_time))
-        
