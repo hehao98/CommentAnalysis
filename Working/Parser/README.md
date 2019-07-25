b32ecb34c0fdb66e84ead2c3ee8423fe0720e67d
Hao He <heh@pku.edu.cn>
2019-07-25 14:39:49 +0800

    Set up environment for comment parser

---
+++ Working/Parser/README.md
@@ -0,0 +1,12 @@
+# CommentParser
+
+The parser program used for comment analysis
+
+## Build
+
+```
+mkdir build
+cd build
+cmake ..
+make
+```
---
+++ Working/Parser/src/Parser.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <tree_sitter/api.h>
+
+TSLanguage *tree_sitter_javascript();
+TSLanguage *tree_sitter_python();
+TSLanguage *tree_sitter_java();
+
+int main(int argc, char **argv)
+{
+
+}
